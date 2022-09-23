d720559452a4e634cd53c9fd0981f3f7b5d2f294
liber09 <113246195+liber09@users.noreply.github.com>
2022-09-23 23:50:14 +0200

    Created the dice class

--- src/Dice.java
+++ src/Dice.java
@@ -1,2 +1,23 @@
+import java.util.Random;
 public class Dice {
+    int currentValue;
+    int numberOfSides;
+    private static Random rnd;
+
+    // creats a dice obeject.
+    public Dice(int numberOfSides){
+        this.numberOfSides = numberOfSides;
+    }
+    //returns the number of sides of the dice.
+    public int getNumberOfSides(){
+       return this.numberOfSides;
+    }
+    // returns the current value of the dice.
+    public int getCurrentValue(){
+        return this.currentValue;
+    }
+    // roll the dice and updates current value.
+    public void roll(){
+        this.currentValue = rnd.nextInt(this.numberOfSides)+1;
+    }
 }
